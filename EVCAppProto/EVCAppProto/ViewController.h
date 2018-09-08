5ded72da09414ddce27919bef405584929816c0e
Mike Laursen <mailbox@laursen.org>
2018-09-08 16:41:18 -0700

    Can now open a sample PDF document in a UIDocumentInteractionController.

--- EVCAppProto/EVCAppProto/ViewController.h
+++ EVCAppProto/EVCAppProto/ViewController.h
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ViewController : UIViewController <UIDocumentInteractionControllerDelegate>
+- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller;
 @end
 
--- EVCAppProto/EVCAppProto/ViewController.m
+++ EVCAppProto/EVCAppProto/ViewController.m
@@ -9,21 +9,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+// TODO: Do we really need an instance variable?
+@private
+UIDocumentInteractionController *docInteractionController;
+}
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)openSampleDocument:(UIButton *)sender {
+    docInteractionController = [[UIDocumentInteractionController alloc] init];
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"97Eurovan" withExtension:@"pdf"];
+    docInteractionController = [UIDocumentInteractionController interactionControllerWithURL:url];
+    docInteractionController.delegate = self;
+    [docInteractionController presentPreviewAnimated:YES];
+}
 
+// MARK: - UIDocumentInteractionControllerDelegate
+
+- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller {
+    return self;
+}
 @end
