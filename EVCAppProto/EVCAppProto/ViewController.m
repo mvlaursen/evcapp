931ded7afaea5379ede1794b0ca0c1ec732bf1e2
Mike Laursen <mailbox@laursen.org>
2018-09-07 17:01:55 -0700

    Generated project

---
+++ EVCAppProto/EVCAppProto/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EVCAppProto
+//
+//  Created by Mike Laursen on 9/7/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
