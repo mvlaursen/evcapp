3e3592745ed0ddfc5c370a7c999a948471638b24
Mike Laursen <mailbox@laursen.org>
2018-09-08 21:20:49 -0700

    Make the main view a table view, with one row per each reference document. This is a barely functional beginning to proper functionality.

---
+++ EVCAppProto/EVCAppProto/TableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.h
+//  EVCAppProto
+//
+//  Created by Mike Laursen on 9/8/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *documentName;
+
+@end
---
+++ EVCAppProto/EVCAppProto/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  EVCAppProto
+//
+//  Created by Mike Laursen on 9/8/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController <UIDocumentInteractionControllerDelegate>
+
+@end
