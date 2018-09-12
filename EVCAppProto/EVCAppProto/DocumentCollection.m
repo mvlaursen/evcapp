51997459c8e3499767c2a01606eee73c35a7276c
Mike Laursen <mailbox@laursen.org>
2018-09-11 21:25:12 -0700

    Added a French Canadian localization.

--- EVCAppProto/EVCAppProto/DocumentCollection.m
+++ EVCAppProto/EVCAppProto/DocumentCollection.m
@@ -13,18 +13,25 @@
 static NSArray *_documents = nil;
 
 + (NSArray *)documents {
-    //    return [[NSBundle mainBundle] URLForResource:@"97Eurovan" withExtension:@"pdf"];
     if (_documents == nil) {
         _documents = [[NSArray alloc] init];
         
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        NSString *documentName = [bundle localizedStringForKey:@"1995EVC" value:@"1995 EuroVan Camper" table:nil];
         NSURL *url = [NSURL URLWithString:@"https://winnebagoind.com/resources/manuals/pdfs/Operator1995/95EuroVan.pdf"];
-        _documents = [_documents arrayByAddingObject:[[Document alloc] init:@"1995 EuroVan Camper" withURL:url]];
+        _documents = [_documents arrayByAddingObject:[[Document alloc] init:documentName withURL:url]];
+        
+        documentName = [bundle localizedStringForKey:@"1995EVC-CA" value:@"1995 EuroVan Camper (Canadian)" table:nil];
         url = [NSURL URLWithString:@"https://winnebagoind.com/resources/manuals/pdfs/Operator1995/95EuroVanCanadian.pdf"];
-        _documents = [_documents arrayByAddingObject:[[Document alloc] init:@"1995 EuroVan Camper (Canadian)" withURL:url]];
+        _documents = [_documents arrayByAddingObject:[[Document alloc] init:documentName withURL:url]];
+        
+        documentName = [bundle localizedStringForKey:@"1997EVC" value:@"1997 EuroVan Camper" table:nil];
         url = [NSURL URLWithString:@"https://winnebagoind.com/resources/manuals/pdfs/Operator1997/97Eurovan.pdf"];
-        _documents = [_documents arrayByAddingObject:[[Document alloc] init:@"1997 EuroVan Camper" withURL:url]];
+        _documents = [_documents arrayByAddingObject:[[Document alloc] init:documentName withURL:url]];
+        
+        documentName = [bundle localizedStringForKey:@"2002EVC" value:@"2002 EuroVan Camper" table:nil];
         url = [NSURL URLWithString:@"https://winnebagoind.com/resources/manuals/pdfs/Operator2002/02Eurovan.pdf"];
-        _documents = [_documents arrayByAddingObject:[[Document alloc] init:@"2002 EuroVan Camper" withURL:url]];
+        _documents = [_documents arrayByAddingObject:[[Document alloc] init:documentName withURL:url]];
     }
     return _documents;
 }
