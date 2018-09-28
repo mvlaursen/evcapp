d32870268d347a87fb161380896b5e03eeb655c2
Mike Laursen <mailbox@laursen.org>
2018-09-28 11:19:03 -0700

    Made the unit tests a little more detailed.

--- EVCApp/EuroVan Camper Handbook/EuroVan Camper HandbookTests/EuroVan_Camper_HandbookTests.swift
+++ EVCApp/EuroVan Camper Handbook/EuroVan Camper HandbookTests/EuroVan_Camper_HandbookTests.swift
@@ -9,6 +9,10 @@
 import XCTest
 @testable import EuroVan_Camper_Handbook
 
+/*
+ * These tests are meant to be run in a scheme that uses English and en_US
+ * locale.
+ */
 class EuroVan_Camper_HandbookTests: XCTestCase {
 
     override func setUp() {
@@ -20,13 +24,22 @@ class EuroVan_Camper_HandbookTests: XCTestCase {
     }
     
     func testValidateReferenceCollection() {
+        // It's unsustainable to have a unit test that repeats every bit of data
+        // in ReferenceCollection, but this is a personal practice project and
+        // the goal is to practice writing unit tests.
+        
         XCTAssert(ReferenceCollection.shared.manuals.count == 4)
         XCTAssert(ReferenceCollection.shared.manuals[0].name == "1995 EuroVan Camper")
+        XCTAssert(ReferenceCollection.shared.manuals[0].url.absoluteString == "https://winnebagoind.com/resources/manuals/pdfs/Operator1995/95EuroVan.pdf")
         XCTAssert(ReferenceCollection.shared.manuals[1].name == "1995 EuroVan Camper (Canadian)")
+        XCTAssert(ReferenceCollection.shared.manuals[1].url.absoluteString == "https://winnebagoind.com/resources/manuals/pdfs/Operator1995/95EuroVanCanadian.pdf")
         XCTAssert(ReferenceCollection.shared.manuals[2].name == "1997 EuroVan Camper")
+        XCTAssert(ReferenceCollection.shared.manuals[2].url.absoluteString == "https://winnebagoind.com/resources/manuals/pdfs/Operator1997/97Eurovan.pdf")
         XCTAssert(ReferenceCollection.shared.manuals[3].name == "2002 EuroVan Camper")
+        XCTAssert(ReferenceCollection.shared.manuals[3].url.absoluteString == "https://winnebagoind.com/resources/manuals/pdfs/Operator2002/02Eurovan.pdf")
         XCTAssert(ReferenceCollection.shared.sites.count == 1)
         XCTAssert(ReferenceCollection.shared.sites[0].name == "Eurovan Rescue")
+        XCTAssert(ReferenceCollection.shared.sites[0].url.absoluteString == "http://www.eurovanrescue.com")
     }
 
     func testPerformanceExample() {
